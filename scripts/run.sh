6aba90c35fa7f4d7d217980f76c38deac0fe39ac
Norm MacLennan <norm.maclennan@gmail.com>
2015-03-30 21:02:53 -0400

    eb wont have bower on path

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-npm install
-bower install
 
-sequelize db:migrate --config data/config/config.json --migrations-path data/migrations --models-path data/models
+npm install
+node_modules/.bin/bower install
+
+node_modules/.bin/sequelize db:migrate --config data/config/config.json --migrations-path data/migrations --models-path data/models
 node server.js 1 prod 127.0.0.1 3000 8081
